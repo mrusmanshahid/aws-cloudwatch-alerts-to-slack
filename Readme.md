bf0a79853c54312d8d0b5da043625c03459674cd
M Usman <m.usman@careem.com>
2021-05-12 18:18:09 +0500

    sample app

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Python script for AWS Lambda to Send Cloudwatch Alerts on Slack
+
+This app delivers cloudwatch alerts to slack using SNS topics subscription.
+
+#### How to deploy?
+
+1. Clone this repository.
+2. Compress (.zip) the entire folder.
+3. Upload on AWS lambda.
+4. Update lambda handler in AWS lambda runtime handler settings to ***lambda.lambda_handler***
+5. Deploy lambda
+6. Subscribe deployed lambda to cloudwatch alert.
+7. We are good! :joy:
---
+++ lambda.py
@@ -0,0 +1,54 @@
+from slack_webhook import Slack
+import json
+
+webhook = "place_your_slack_webhook_here"
+
+def post_message_to_slack(json_msg):
+    slack = Slack(url=webhook)
+    slack.post(text='Cloud Watch Alert',
+               as_user= True,
+               username="Cloud Watch Alerts",
+               attachments=[{
+                   "fallback": json_msg,
+                   "message": json_msg,
+                   "color": "danger",
+                   "fields": [
+                       {
+                           "title": "Metric",
+                           "value": json_msg['Trigger']['MetricName'],
+                           "short": False
+                       },
+                       {
+                           "title": "Status",
+                           "value": json_msg['NewStateValue'],
+                           "short": True
+                       },
+                       {
+                           "title": "State Change Time",
+                           "value": json_msg['StateChangeTime'],
+                           "short": True
+                       },
+                       {
+                           "title": "Instance",
+                           "value": json_msg['Trigger']['Dimensions'][0]['value'],
+                           "short": True
+                       },
+                       {
+                           "title": "Alarm Name",
+                           "value": json_msg['AlarmName'],
+                           "short": False
+                       },
+                       {
+                           "title": "Reason",
+                           "value": json_msg['NewStateReason'],
+                           "short": False
+                       },
+                   ]
+               }]
+               )
+
+def lambda_handler(event, context):
+    print(event)
+    sns = event['Records'][0]['body']
+    json_msg = json.loads(json.loads(sns)['Message'])
+    post_message_to_slack(json_msg)
